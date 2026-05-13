29736cf47dea3110421dbe29a64ee1bec4dbf895
omart <omartama1213@gmail.com>
2026-05-13 11:32:14 -0500

    se añadio la encriptacion de la contraseña con bcrypt

--- backend/models/veterinary.js
+++ backend/models/veterinary.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import generateId from "../helpers/generateId.js";
 
 const veterinarySchema = new mongoose.Schema({
     name: {
@@ -33,6 +35,16 @@ const veterinarySchema = new mongoose.Schema({
         type: Boolean,
         default: false,
     }
+});
+
+// Middleware para hashear la contraseña antes de guardar el veterinario
+veterinarySchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 const Veterinary = mongoose.model('Veterinary', veterinarySchema);
